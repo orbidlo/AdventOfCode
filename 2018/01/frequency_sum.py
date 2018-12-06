de190914804872e9d007c5c958c686af7d179204
orbidlo <orbidlo@gmail.com>
2018-12-06 22:32:57 +0100

    https://adventofcode.com/2018/day/1

---
+++ 2018/01/frequency_sum.py
@@ -0,0 +1,61 @@
+# https://adventofcode.com/2018/day/1
+
+from itertools import accumulate
+import os.path
+
+STARTING_FREQUENCY = 0
+INPUT_FILE = 'input.txt'
+INPUT_TEST = 'input_test.txt'
+
+
+def list_forever(l):
+    while True:
+        yield from l
+
+
+def get_frequency_list(input_file):
+    with open(input_file) as f:
+        frequency_list = list(map(int, f))
+    return frequency_list
+
+
+# part 1
+def get_frequency(input_file):
+    frequency_list = get_frequency_list(input_file)
+    return len(list(frequency_list)), sum(frequency_list)
+
+
+def test_get_frequency():
+    count, result = get_frequency(os.path.join(os.path.dirname(__file__), INPUT_TEST))
+    assert count == 4
+    assert result == 3
+
+
+# part 2
+def get_repeat_frequency(input_file):
+    frequency_list = get_frequency_list(input_file)
+    duplicate_found = None
+    found_frequencies = {STARTING_FREQUENCY}
+    candidates = list_forever(frequency_list)
+
+    for frequency in accumulate(candidates):
+        if frequency in found_frequencies:
+            duplicate_found = frequency
+            break
+        else:
+            found_frequencies.add(frequency)
+
+    return duplicate_found
+
+
+def test_get_repeat_frequency():
+    result = get_repeat_frequency(os.path.join(os.path.dirname(__file__), INPUT_TEST))
+    assert result == 2
+
+
+if __name__ == "__main__":
+    count, final_frequency = get_frequency(INPUT_FILE)
+    print(f'Sum of frequency for {count} numbers is {final_frequency}')
+
+    duplicate_frequency = get_repeat_frequency(INPUT_FILE)
+    print(f'Duplicate frequency is {duplicate_frequency}')
