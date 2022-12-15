29e4b4d6cb0435f8afe3f088616ae752cefca648
=orbidlo <andrea.patakova@profinit.eu>
2022-12-15 10:09:59 +0100

    change library timeit decorator to show milliseconds.

--- libs.py
+++ libs.py
@@ -8,11 +8,11 @@ INPUT_TEST = 'input_test.txt'
 def timeit(func):
     @wraps(func)
     def timeit_wrapper(*args, **kwargs):
-        start_time = time.perf_counter_ns()
+        start_time = time.perf_counter()
         result = func(*args, **kwargs)
-        end_time = time.perf_counter_ns()
+        end_time = time.perf_counter()
         total_time = end_time - start_time
-        print(f'\tFunction {func.__name__} took {total_time / 1000} μs')
+        print(f'\tFunction {func.__name__} took {total_time * 1000:.2f} ms')
         return result
 
     return timeit_wrapper
